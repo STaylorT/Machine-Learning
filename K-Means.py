31673289c120a052dbee21d35d809f75b01e2ceb
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-09-10 17:36:56 -0400

    Update K-Means.py
    Current abilities:
    - Find initial k cluster seeds
    - Sort elements of data set into clusters according to initial cluster seeds
    - Find mean of each of k clusters
    - Use mean to obtain new cluster seeds
    Developing: Iterating method to repeat above steps
    - until no change in clusters / max iterations

--- K-Means.py
+++ K-Means.py
@@ -5,15 +5,15 @@ import sys
 dimensions = 2
 
 
-def compute_distance(point, centroids):
+def compute_centroid(element, centroids):
+    """ return the index of the closest centroid to given element"""
     which_centroid = 0
     min_dist = sys.maxsize
 
-    # print(min_dist)
     for centroid in centroids:
         dist = 0  # temp dist
         for i in range(dimensions):
-            dist += (point[i] - centroid[i]) ** 2
+            dist += (element[i] - centroid[i]) ** 2
         if dist < min_dist:  # new min distance
             which_centroid = centroids.index(centroid)  # index of closest centroid
             min_dist = dist
@@ -22,8 +22,13 @@ def compute_distance(point, centroids):
 
 def compute_cluster_mean(cluster):
     mean_element = list(cluster[0])
-    print(mean_element)
+    for dim in range(dimensions):
+        for element in cluster:
+            mean_element[dim] += element[dim]  # Sum of elements' "dim" dimension
 
+        # Computing Average for each dimension (dividing by num elements)
+        mean_element[dim] /= len(cluster)
+    return mean_element  # return average
 
 
 dataset = [[1, 1], [1, 4], [3, 4], [2, 2], [2, 3], [2, 1], [-1, -2], [-1, -4], [-4, -1], [-2, -1], [-3, -2]]
@@ -48,10 +53,13 @@ for x in range(k):
 # Calculate distance from each element in dataset to each cluster seed
 # And choose which of k clusters is closest to this element
 for point in dataset:
-    closest_centroid_index = compute_distance(point, centroids)
+    closest_centroid_index = compute_centroid(point, centroids)
     clusters[closest_centroid_index].append(point)  # grouping each point into a cluster
 
 # Finding new centroid for each cluster
-compute_cluster_mean(clusters[0])
-print("\n Here is the cluster vector: ", clusters)
+for cluster_k in clusters:
+    average_of_cluster = compute_cluster_mean(cluster_k)  # literal average, not necessarily an element in cluster
+    new_centroid = cluster_k[compute_centroid(average_of_cluster, cluster_k)]  # find new centroid (closest element to avg)
+    print("\n Here is the cluster vector: ", clusters)
+    print ("\n New centroid: ", new_centroid)
 
